e15b3886180e9c080173b55d409f62b43f84d99b
krishnapal85816-dev <krishnapal85816@gmail.com>
2026-09-02 07:11:05 +0530

    Time: 3 ms (76.96%) | Memory: 45.7 MB (99.48%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,32 +1,27 @@
 class Solution {
     public int romanToInt(String s) {
-        int total = 0;
+        int[] values = new int[128];
+
+        values['I'] = 1;
+        values['V'] = 5;
+        values['X'] = 10;
+        values['L'] = 50;
+        values['C'] = 100;
+        values['D'] = 500;
+        values['M'] = 1000;
+
+        int result = 0;
 
         for (int i = 0; i < s.length(); i++) {
-            int current = value(s.charAt(i));
+            int current = values[s.charAt(i)];
 
-            // If current value is smaller than the next value,
-            // subtract it (e.g., IV = 5 - 1).
-            if (i + 1 < s.length() && current < value(s.charAt(i + 1))) {
-                total -= current;
+            if (i + 1 < s.length() && current < values[s.charAt(i + 1)]) {
+                result -= current;
             } else {
-                total += current;
+                result += current;
             }
         }
 
-        return total;
-    }
-
-    private int value(char c) {
-        switch (c) {
-            case 'I': return 1;
-            case 'V': return 5;
-            case 'X': return 10;
-            case 'L': return 50;
-            case 'C': return 100;
-            case 'D': return 500;
-            case 'M': return 1000;
-            default: return 0;
-        }
+        return result;
     }
 }
